ab298eb7827accb5f73d8beadde0d0c2eb48f943
Ian-Hodges <ian.hodges@bjss.com>
2021-12-06 10:11:22 +0000

    feat(cb2-1310): extra unit tests

--- tests/unit/exportevents.test.ts
+++ tests/unit/exportevents.test.ts
@@ -1,19 +1,16 @@
 import { mocked } from 'ts-jest/utils';
 import { Database } from '../../src/wms/Database';
 import { getEvents } from '../../src/wms/ExportEvents';
+import { StaffSchedule } from '../../src/wms/Interfaces/StaffSchedule';
 
 jest.mock('../../src/wms/Database');
 const mDatabase = mocked(Database, true);
-const ent = {
-  site_id: '123',
-  staff_id: 2,
-  status: 'New',
-  event_date: '2021-10-20',
-  event_start: '09:00',
-  event_end: '10:00',
-};
+const schedule1 = getSchedule('site1', 1);
+const schedule2 = getSchedule('site1', 2);
+const schedule3 = getSchedule('site2', 3);
+
 let mDatabaseImp = ({
-  getstaffSchedules: jest.fn().mockResolvedValue([ent]),
+  getstaffSchedules: jest.fn().mockResolvedValue([schedule1, schedule2, schedule3]),
   closeConnection: jest.fn().mockImplementation(() => Promise.resolve()),
 });
 
@@ -25,9 +22,22 @@ const exportDate = new Date('2021-10-10T10:10:10.000Z');
 
 describe('Export events', () => {
   describe('getEvents', () => {
-    it('GIVEN one schedule returned from database WHEN processed THEN one order is returned.', async () => {
+    it('GIVEN three schedules returned for two distinct sites WHEN processed THEN two orders are returned.', async () => {
       const schedules = await getEvents(exportDate);
-      expect(schedules).toHaveLength(1);
+      expect(schedules).toHaveLength(2);
+    });
+
+    it('GIVEN two schedules returned for one site WHEN processed THEN one order has two VSAs.', async () => {
+      const schedules = await getEvents(exportDate);
+      expect(schedules[0].vsa).toHaveLength(2);
+      expect(schedules[0].vsa[0].testerid).toEqual(1);
+      expect(schedules[0].vsa[1].testerid).toEqual(2);
+    });
+
+    it('GIVEN one schedule returned for one site WHEN processed THEN one order has one VSA.', async () => {
+      const schedules = await getEvents(exportDate);
+      expect(schedules[1].vsa).toHaveLength(1);
+      expect(schedules[1].vsa[0].testerid).toEqual(3);
     });
 
     it('GIVEN a call to the database WHEN an error from the database occurs THEN getEvents returns an error.', async () => {
@@ -59,3 +69,14 @@ describe('Export events', () => {
     });
   });
 });
+
+function getSchedule(cId: string, staffId: number): StaffSchedule {
+  return {
+    c_id: cId,
+    staff_id: staffId,
+    status: 'New',
+    event_date: '2021-10-20',
+    event_start: '11:00',
+    event_end: '13:00',
+  };
+}
