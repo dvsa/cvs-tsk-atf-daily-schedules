6432e030f9ba954b8664973e9812b86328913f6a
ian-hodges <ian.hodges@bjss.com>
2022-01-20 13:55:07 +0000

    refactor(cb2-2242): move date validation, and correct implementation of the callback unit tests

--- src/handler.ts
+++ src/handler.ts
@@ -16,32 +16,16 @@ logger.debug(
 const handler = (event: ScheduledEvent, _context: Context, callback: Callback) => {
   logger.debug(`Function triggered with '${JSON.stringify(event)}'.`);
 
-  let exportDate: Date;
-  if (event?.detail?.exportDate) {
-    exportDate = getDateFromManualTrigger(event.detail.exportDate as string);
-  } else {
-    exportDate = new Date(Date.now());
-  }
-
-  getEvents(exportDate)
-    .then((facilitySchedules) => sendEvents(facilitySchedules)
-      .then(() => {
-        logger.info('Data processed successfully.');
-        callback(null, 'Data processed successfully.');
-      }))
+  getEvents(event?.detail?.exportDate as string)
+    .then((facilitySchedules) => sendEvents(facilitySchedules).then(() => {
+      logger.info('Data processed successfully.');
+      callback(null, 'Data processed successfully.');
+    }))
     .catch((error) => {
       logger.info('Data processed unsuccessfully.');
       logger.error('', error);
       callback(new Error('Data processed unsuccessfully.'));
     });
 };
-
-function getDateFromManualTrigger(inputDate: string): Date {
-  const isValidDate = !Number.isNaN(Date.parse(inputDate));
-  if (!isValidDate) {
-    throw new Error(`Failed to manually trigger function. Invalid input date ${inputDate}`);
-  }
-  return new Date(inputDate);
-}
 
 export { handler };
