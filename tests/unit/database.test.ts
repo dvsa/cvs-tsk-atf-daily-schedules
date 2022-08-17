5d7fef241a638f2567ef59103e1857c8c53ab124
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-08-17 11:09:30 +0100

    feat(cb2-1111): fix failing unit tests

--- tests/unit/database.test.ts
+++ tests/unit/database.test.ts
@@ -64,12 +64,8 @@ describe('Database calls', () => {
         'EVENT_START',
         'EVENT_END',
       );
-      expect(mKnex.innerJoin).toBeCalledWith('NGT_STAFF', 'NGT_SITE_EVENTS.STAFF_ID', 'NGT_STAFF.STAFF_ID');
-      expect(mKnex.innerJoin).toBeCalledWith('NGT_SITE', 'NGT_SITE_EVENTS.SITE_ID', 'NGT_SITE.SITE_ID');
-      expect(mKnex.from).toBeCalledWith('NGT_SITE_EVENTS');
+      expect(mKnex.from).toBeCalledWith('Daily_Booking_Alterations');
       expect(mKnex.where).toBeCalledWith('EVENT_DATE', '=', '2021-10-10');
-      expect(mKnex.where).toBeCalledWith('STATUS', 'ALLOCATED');
-      expect(mKnex.where).toBeCalledWith('NGT_SITE_EVENTS.DELETED', 0);
       expect(mKnex.havingIn).toBeCalledWith('NGT_SITE.C_ID', ['100', '101']);
       expect(staffSchedules).toHaveLength(1);
     });
@@ -77,7 +73,6 @@ describe('Database calls', () => {
     it('GIVEN a call to getstaffSchedules WHEN empty list returned THEN error thrown.', async () => {
       mKnex = ({
         select: jest.fn().mockReturnThis(),
-        innerJoin: jest.fn().mockReturnThis(),
         from: jest.fn().mockReturnThis(),
         where: jest.fn().mockReturnThis(),
         havingIn: jest.fn(() => []),
@@ -102,11 +97,8 @@ describe('Database calls', () => {
         'EVENT_START',
         'EVENT_END',
       );
-      expect(mKnex.innerJoin).toBeCalledWith('NGT_STAFF', 'NGT_SITE_EVENTS.STAFF_ID', 'NGT_STAFF.STAFF_ID');
-      expect(mKnex.innerJoin).toBeCalledWith('NGT_SITE', 'NGT_SITE_EVENTS.SITE_ID', 'NGT_SITE.SITE_ID');
-      expect(mKnex.from).toBeCalledWith('NGT_SITE_EVENTS');
+      expect(mKnex.from).toBeCalledWith('Daily_Booking_Alterations');
       expect(mKnex.where).toBeCalledWith('EVENT_DATE', '=', '2021-10-10');
-      expect(mKnex.where).toBeCalledWith('STATUS', 'ALLOCATED');
       expect(mKnex.havingIn).toBeCalledWith('NGT_SITE.C_ID', ['100', '101']);
     });
 
