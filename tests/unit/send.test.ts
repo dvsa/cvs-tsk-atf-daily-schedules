7cdd4f9dad9c23c4b7c673f4ab6d0bd75f12963d
Owen Corrigan (BJSS, VTM) <122352145+owen-corrigan-bjss@users.noreply.github.com>
2024-04-30 13:01:55 +0100

    chore(cb2-11265): refactor tests to be more like they were previously

--- tests/unit/send.test.ts
+++ tests/unit/send.test.ts
@@ -7,6 +7,16 @@ import { sendEvents } from '../../src/eventbridge/send';
 import { FacillitySchedules } from '../../src/wms/Interfaces/DynamicsCE';
 
 const eventBridgeMock = mockClient(EventBridgeClient);
+const mockPut = (params: { Entries: [{ Detail }] }) => {
+  const mPutEventsResponse = {
+    FailedEntryCount: 0,
+    Entries: Array<PutEventsResultEntry>(params.Entries.length),
+  };
+  if (params.Entries[0].Detail === '{ "schedule": "{\\"testfacilityid\\":\\"Error\\",\\"eventdate\\":\\"Now\\"}" }') {
+    return Promise.reject(new Error('Oh no!'));
+  }
+  return Promise.resolve(mPutEventsResponse);
+};
 
 describe('Send events', () => {
   describe('Events sent', () => {
@@ -16,33 +26,21 @@ describe('Send events', () => {
     it('GIVEN one event to send WHEN sent THEN one event is returned.', async () => {
       const mFacillitySchedules = Array<FacillitySchedules>(1);
       const mSendResponse: SendResponse = { SuccessCount: 1, FailCount: 0 };
-      eventBridgeMock.on(PutEventsCommand).resolves({
-        FailedEntryCount: 0,
-        Entries: Array<PutEventsResultEntry>(1),
-      });
+      eventBridgeMock.on(PutEventsCommand).callsFake((params: { Entries: [{ Detail }] }) => mockPut(params));
       await expect(sendEvents(mFacillitySchedules)).resolves.toEqual(mSendResponse);
     });
 
     it('GIVEN two events to send WHEN sent THEN two events are returned.', async () => {
       const mFacillitySchedules = Array<FacillitySchedules>(2);
       const mSendResponse: SendResponse = { SuccessCount: 2, FailCount: 0 };
-      eventBridgeMock.on(PutEventsCommand).resolves({
-        FailedEntryCount: 0,
-        Entries: Array<PutEventsResultEntry>(2),
-      });
+      eventBridgeMock.on(PutEventsCommand).callsFake((params: { Entries: [{ Detail }] }) => mockPut(params));
       await expect(sendEvents(mFacillitySchedules)).resolves.toEqual(mSendResponse);
     });
 
     it('GIVEN an issue with eventbridge WHEN 6 events are sent and 1 fails THEN the failure is in the response.', async () => {
       const mFacillitySchedules = Array<FacillitySchedules>(6);
       mFacillitySchedules[0] = { testfacilityid: 'Error', eventdate: 'Now' };
-      eventBridgeMock.on(PutEventsCommand).callsFake((params: { Entries: [{ Detail }] }) => {
-        if (params.Entries[0].Detail !== '{ "schedule": "undefined" }') {
-          return new Error('oh no');
-        }
-        return { Entries: [{}] };
-      });
-
+      eventBridgeMock.on(PutEventsCommand).callsFake((params: { Entries: [{ Detail }] }) => mockPut(params));
       const mSendResponse: SendResponse = { SuccessCount: 5, FailCount: 1 };
       await expect(sendEvents(mFacillitySchedules)).resolves.toEqual(mSendResponse);
     });
